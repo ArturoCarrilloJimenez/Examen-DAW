d5dfe570e97910b2a4bab931378dba0899b965d6
ArturoCarrilloJimenez <arturojimenezalmontaras@gmail.com>
2024-12-12 13:32:10 +0000

    fin deploy en teoria

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,10 +13,32 @@ wget https://github.com/moodle/moodle/archive/refs/tags/v4.5.1.zip -P /tmp
 
 apt install unzip
 
+rm -rf /tmp/moodle-4.5.1
+
 unzip /tmp/v4.5.1.zip -d /tmp
 
 rm -rf /var/www/html/*
 
-mv -f /tmp/moodle-4.5.1/* /var/www/html
+mv /tmp/moodle-4.5.1/* /var/www/html/
 
+mkdir -p /var/www/moodledata/
 
+chown -R www-data:www-data /var/www/moodledata
+
+sudo -u www-data php /var/www/html/admin/cli/install.php \
+    --lang=$MOODLE_LANG \
+    --wwwroot=$MOODLE_WWWROOT \
+    --dataroot=$MOODLE_DAAROOT \
+    --dbtype=$MOODLE_DB_TYPE \
+    --dbhost=$MOODLE_DB_HOST \
+    --dbname=$MOODLE_DB_NAME \
+    --dbuser=$MOODLE_DB_USER \
+    --dbpass=$MOODLE_DB_PASS \
+    --fullname="$MOODLE_FULLNAME" \
+    --shortname="$MOODLE_SHORTNAME" \
+    --summary="$MOODLE_SUMARY" \
+    --adminuser=$MOODLE_ADMIN_USER \
+    --adminpass=$MOODLE_ADMIN_PASS \
+    --adminemail=$MOODLE_ADMIN_EMAIL \
+    --non-interactive \
+    --agree-license
